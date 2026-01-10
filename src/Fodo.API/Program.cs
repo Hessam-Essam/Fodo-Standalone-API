2038b3b7e203972bc802eea894de6b27df31cfa6
Hesham Essam <hessam@youxel.com>
2026-01-10 13:37:41 +0200

    Enabling swagger in API

--- src/Fodo.API/Program.cs
+++ src/Fodo.API/Program.cs
@@ -1,8 +1,20 @@
+using Microsoft.OpenApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "My API",
+        Version = "v1",
+        Description = ".NET 10 Clean Architecture API"
+    });
+});
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -11,6 +23,12 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
+        options.RoutePrefix = "swagger"; // URL: /swagger
+    });
     app.MapOpenApi();
 }
 
